58929cdc15fdca5c01eea0b2eeafc3fb7c7f9fa9
Lily Donaldson <lilykdonaldson@gmail.com>
2019-10-09 19:50:28 -0400

    added grants

---
+++ grants.sql
@@ -0,0 +1,25 @@
+--
+-- PostgreSQL database dump
+--
+
+SET statement_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = off;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+SET escape_string_warning = off;
+
+SET search_path = public, pg_catalog;
+
+--
+-- Data for Name: Grants; Type: TABLE DATA; Schema: public; Owner: lilykdon
+--
+
+COPY "Grants" ("Name", "Provider", "Deadline", "Type", "Amount", "Grant Categories") FROM stdin;
+\.
+
+
+--
+-- PostgreSQL database dump complete
+--
+
